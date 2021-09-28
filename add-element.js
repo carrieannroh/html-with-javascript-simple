c2d42e052dc92b6bfa127e532614840e05d87056
Carrie <carriearoberts95@gmail.com>
2021-09-28 11:45:56 +0100

    adding changes

--- add-element.js
+++ add-element.js
@@ -1,12 +1,16 @@
 // create a new div element
-const newDiv = document.createElement("div");
-
+//const newDiv = document.createElement("div");
+const h2 = document.createElement("h2");
 // and give it some content
-const newContent = document.createTextNode("Hi there and greetings!");
+//const newContent = document.createTextNode("Hi there and greetings!");
+const newHead = document.createTextNode("Greetings!");
 
 // add the text node to the newly created div
-newDiv.appendChild(newContent);
-
+//newDiv.appendChild(newContent);
+newDiv.appendChild(newHead);
 // add the newly created element and its content into the DOM
+//const existingDiv = document.getElementById("my-target-element");
+//document.body.appendChild(newDiv, existingDiv);
+
 const existingDiv = document.getElementById("my-target-element");
-document.body.appendChild(newDiv, existingDiv);
+document.body.appendChild(newDiv, existingDiv, h2);
